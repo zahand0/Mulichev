1f59a0d388a4bec2154e489cd9bb3ffcead2cfc7
Sergey <mulichev@gmail.com>
2023-02-04 22:58:35 +0300

    added more constants

--- app/src/main/java/com/zahand0/moviesearch/util/Constants.kt
+++ app/src/main/java/com/zahand0/moviesearch/util/Constants.kt
@@ -1,5 +1,15 @@
 package com.zahand0.moviesearch.util
 
 object Constants {
+
+    const val BASE_URL = "https://kinopoiskapiunofficial.tech"
+    const val API_KEY = "e30ffed0-76ab-4dd6-b41f-4c9da2b2735b"
+
+    const val FILM_DATABASE_NAME = "film_database"
+    const val FILM_DATABASE_TABLE = "film_table"
+    const val FILM_REMOTE_KEYS_DATABASE_TABLE = "film_remote_keys_table"
+
+    const val ITEMS_PER_PAGE = 10
+
     const val MOVIE_SCREEN_ARGUMENT_KEY = "movie_id"
 }
