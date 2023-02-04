1b05ee3507074bde269a5a2b6bf9b56a257ffe8c
Sergey <mulichev@gmail.com>
2023-02-04 22:55:50 +0300

    implemented api

---
+++ app/src/main/java/com/zahand0/moviesearch/data/remote/KinopoiskUnofficialAPI.kt
@@ -0,0 +1,22 @@
+package com.zahand0.moviesearch.data.remote
+
+import com.zahand0.moviesearch.domain.model.Film
+import com.zahand0.moviesearch.domain.model.FilmListResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface KinopoiskUnofficialAPI {
+
+    @GET("/api/v2.2/films/top")
+    suspend fun getAllFilms(
+        @Query("page") page: Int = 1,
+        @Query("type") type: String = "TOP_100_POPULAR_FILMS",
+    ): FilmListResponse
+
+    @GET(" /api/v2.2/films/top/{id}")
+    suspend fun getFilm(
+        @Path("id") id: Int
+    ): Film
+
+}
---
+++ app/src/main/java/com/zahand0/moviesearch/di/NetworkModule.kt
@@ -0,0 +1,69 @@
+package com.zahand0.moviesearch.di
+
+import androidx.paging.ExperimentalPagingApi
+import com.zahand0.moviesearch.data.local.FilmDatabase
+import com.zahand0.moviesearch.data.remote.KinopoiskUnofficialAPI
+import com.zahand0.moviesearch.data.repository.RemoteDataSourceImpl
+import com.zahand0.moviesearch.domain.repository.RemoteDataSource
+import com.zahand0.moviesearch.util.Constants
+import com.zahand0.moviesearch.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@ExperimentalPagingApi
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+
+        return OkHttpClient.Builder().apply {
+            addInterceptor(
+                Interceptor { chain ->
+                    val builder = chain.request().newBuilder()
+                    builder.header("X-API-KEY", Constants.API_KEY)
+                    return@Interceptor chain.proceed(builder.build())
+                }
+            )
+        }.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(okHttpClient: OkHttpClient): Retrofit {
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideKinopoiskUnofficialApi(retrofit: Retrofit): KinopoiskUnofficialAPI {
+        return retrofit.create(KinopoiskUnofficialAPI::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRemoteDataSource(
+        kinopoiskUnofficialAPI: KinopoiskUnofficialAPI,
+        filmDatabase: FilmDatabase
+    ): RemoteDataSource {
+        return RemoteDataSourceImpl(
+            kinopoiskUnofficialAPI = kinopoiskUnofficialAPI,
+            filmDatabase = filmDatabase
+        )
+    }
+}
